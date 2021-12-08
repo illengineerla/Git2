abba7c1376ded6de9f8e7e4f8cd1078a03184d4d
illengineerla <ill.la.kinin@gmail.com>
2021-12-08 21:41:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/git2/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.git2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends Activity {
+
+    private ProgressBar mProgressBar;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+
+    public void onClick(View view) {
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+        final Call<User> call =
+                gitHubService.getUser("alexanderklimov");
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    User user = response.body();
+
+                    mTextView.setText("Аккаунт Github: " + user.getName() +
+                            "\nСайт: " + user.getBlog() +
+                            "\nКомпания: " + user.getCompany());
+
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    int statusCode = response.code();
+
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        mTextView.setText(errorBody.string());
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable throwable) {
+                mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+            }
+        });
+    }
+}
